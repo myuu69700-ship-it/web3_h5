42901deb47b29f750b6620891590f6fd9dddd822
web-h-002 <web-h-002@gmail.com>
2026-01-05 10:49:44 +0800

    首页底部组件拆分优化

---
+++ src/components/Home/MenuSection.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="menu-section">
+    <!-- 交易市場 -->
+    <div class="menu-item">
+      <div class="menu-header" @click="toggleMenu('tradingMarket')">
+        <span class="menu-title">{{ t("tradingMarket") }}</span>
+        <span class="menu-toggle">{{ expandedMenus.tradingMarket ? '−' : '+' }}</span>
+      </div>
+      <div v-if="expandedMenus.tradingMarket" class="menu-content">
+        <div class="menu-sub-item">{{ t("spot") }}</div>
+        <div class="menu-sub-item">{{ t("perpetual") }}</div>
+        <div class="menu-sub-item">{{ t("options") }}</div>
+      </div>
+    </div>
+    
+    <!-- 資產管理 -->
+    <div class="menu-item">
+      <div class="menu-header" @click="toggleMenu('assetManagement')">
+        <span class="menu-title">{{ t("assetManagement") }}</span>
+        <span class="menu-toggle">{{ expandedMenus.assetManagement ? '−' : '+' }}</span>
+      </div>
+      <div v-if="expandedMenus.assetManagement" class="menu-content">
+        <div class="menu-sub-item">{{ t("myAssets") }}</div>
+        <div class="menu-sub-item">{{ t("deposit") }}</div>
+        <div class="menu-sub-item">{{ t("withdraw") }}</div>
+        <div class="menu-sub-item">{{ t("transfer") }}</div>
+        <div class="menu-sub-item">{{ t("orderCenter") }}</div>
+      </div>
+    </div>
+    
+    <!-- 金融理財 -->
+    <div class="menu-item">
+      <div class="menu-header" @click="toggleMenu('financialManagement')">
+        <span class="menu-title">{{ t("financialManagement") }}</span>
+        <span class="menu-toggle">{{ expandedMenus.financialManagement ? '−' : '+' }}</span>
+      </div>
+      <div v-if="expandedMenus.financialManagement" class="menu-content">
+        <div class="menu-sub-item">{{ t("products") }}</div>
+      </div>
+    </div>
+    
+    <!-- 支持 -->
+    <div class="menu-item">
+      <div class="menu-header" @click="toggleMenu('support')">
+        <span class="menu-title">{{ t("support") }}</span>
+        <span class="menu-toggle">{{ expandedMenus.support ? '−' : '+' }}</span>
+      </div>
+      <div v-if="expandedMenus.support" class="menu-content">
+        <div class="menu-sub-item">{{ t("newbieTutorial") }}</div>
+        <div class="menu-sub-item">{{ t("consultationService") }}: 0</div>
+      </div>
+    </div>
+    
+    <!-- 政策 -->
+    <div class="menu-item">
+      <div class="menu-header" @click="toggleMenu('policies')">
+        <span class="menu-title">{{ t("policies") }}</span>
+        <span class="menu-toggle">{{ expandedMenus.policies ? '−' : '+' }}</span>
+      </div>
+      <div v-if="expandedMenus.policies" class="menu-content">
+        <div class="menu-sub-item">{{ t("termsOfService") }}</div>
+        <div class="menu-sub-item">{{ t("privacyPolicy") }}</div>
+        <div class="menu-sub-item">{{ t("antiMoneyLaundering") }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useI18n } from "@/i18n";
+
+const { t } = useI18n();
+
+// 菜单展开状态
+const expandedMenus = ref({
+  tradingMarket: false,
+  assetManagement: false,
+  financialManagement: false,
+  support: false,
+  policies: false
+});
+
+// 切换菜单展开/折叠
+const toggleMenu = (menuName) => {
+  expandedMenus.value[menuName] = !expandedMenus.value[menuName];
+};
+</script>
+
+<style lang="scss" scoped>
+.menu-section {
+  margin-top: 16px;
+
+  .menu-item {
+    border-bottom: 1px solid #ebedf0;
+
+    &:last-child {
+      border-bottom: none;
+    }
+
+    .menu-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 16px;
+      cursor: pointer;
+      user-select: none;
+
+      .menu-title {
+        font-size: 16px;
+        font-weight: 500;
+        color: #323233;
+      }
+
+      .menu-toggle {
+        font-size: 20px;
+        color: #323233;
+        font-weight: 300;
+        line-height: 1;
+      }
+    }
+
+    .menu-content {
+      padding: 0 16px 16px 16px;
+
+      .menu-sub-item {
+        padding: 12px 0;
+        font-size: 14px;
+        color: #323233;
+        border-bottom: 1px solid #ebedf0;
+
+        &:last-child {
+          border-bottom: none;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -217,71 +217,7 @@
         </div>
         
         <!-- 菜单折叠面板 -->
-        <div class="menu-section">
-          <!-- 交易市場 -->
-          <div class="menu-item">
-            <div class="menu-header" @click="toggleMenu('tradingMarket')">
-              <span class="menu-title">{{ t("tradingMarket") }}</span>
-              <span class="menu-toggle">{{ expandedMenus.tradingMarket ? '−' : '+' }}</span>
-            </div>
-            <div v-if="expandedMenus.tradingMarket" class="menu-content">
-              <div class="menu-sub-item">{{ t("spot") }}</div>
-              <div class="menu-sub-item">{{ t("perpetual") }}</div>
-              <div class="menu-sub-item">{{ t("options") }}</div>
-            </div>
-          </div>
-          
-          <!-- 資產管理 -->
-          <div class="menu-item">
-            <div class="menu-header" @click="toggleMenu('assetManagement')">
-              <span class="menu-title">{{ t("assetManagement") }}</span>
-              <span class="menu-toggle">{{ expandedMenus.assetManagement ? '−' : '+' }}</span>
-            </div>
-            <div v-if="expandedMenus.assetManagement" class="menu-content">
-              <div class="menu-sub-item">{{ t("myAssets") }}</div>
-              <div class="menu-sub-item">{{ t("deposit") }}</div>
-              <div class="menu-sub-item">{{ t("withdraw") }}</div>
-              <div class="menu-sub-item">{{ t("transfer") }}</div>
-              <div class="menu-sub-item">{{ t("orderCenter") }}</div>
-            </div>
-          </div>
-          
-          <!-- 金融理財 -->
-          <div class="menu-item">
-            <div class="menu-header" @click="toggleMenu('financialManagement')">
-              <span class="menu-title">{{ t("financialManagement") }}</span>
-              <span class="menu-toggle">{{ expandedMenus.financialManagement ? '−' : '+' }}</span>
-            </div>
-            <div v-if="expandedMenus.financialManagement" class="menu-content">
-              <div class="menu-sub-item">{{ t("products") }}</div>
-            </div>
-          </div>
-          
-          <!-- 支持 -->
-          <div class="menu-item">
-            <div class="menu-header" @click="toggleMenu('support')">
-              <span class="menu-title">{{ t("support") }}</span>
-              <span class="menu-toggle">{{ expandedMenus.support ? '−' : '+' }}</span>
-            </div>
-            <div v-if="expandedMenus.support" class="menu-content">
-              <div class="menu-sub-item">{{ t("newbieTutorial") }}</div>
-              <div class="menu-sub-item">{{ t("consultationService") }}: 0</div>
-            </div>
-          </div>
-          
-          <!-- 政策 -->
-          <div class="menu-item">
-            <div class="menu-header" @click="toggleMenu('policies')">
-              <span class="menu-title">{{ t("policies") }}</span>
-              <span class="menu-toggle">{{ expandedMenus.policies ? '−' : '+' }}</span>
-            </div>
-            <div v-if="expandedMenus.policies" class="menu-content">
-              <div class="menu-sub-item">{{ t("termsOfService") }}</div>
-              <div class="menu-sub-item">{{ t("privacyPolicy") }}</div>
-              <div class="menu-sub-item">{{ t("antiMoneyLaundering") }}</div>
-            </div>
-          </div>
-        </div>
+        <MenuSection />
       </div>
     </div>
 
@@ -357,6 +293,7 @@ import chartIcon from "@/assets/images/chart.png";
 import nomoreIcon from "@/assets/images/nomore.png";
 import nftIcon from "@/assets/images/nft.png";
 import zhusImage from "@/assets/images/zhus.png";
+import MenuSection from "@/components/Home/MenuSection.vue";
 
 const router = useRouter();
 const { t, currentLang, setLanguage } = useI18n();
@@ -364,20 +301,6 @@ const showMenu = ref(false);
 const showLanguageDialog = ref(false);
 const searchValue = ref("");
 const activeTab = ref("favorites"); // 默认显示自選
-
-// 菜单展开状态
-const expandedMenus = ref({
-  tradingMarket: false,
-  assetManagement: false,
-  financialManagement: false,
-  support: false,
-  policies: false
-});
-
-// 切换菜单展开/折叠
-const toggleMenu = (menuName) => {
-  expandedMenus.value[menuName] = !expandedMenus.value[menuName];
-};
 
 // 自選数据（空数组，显示暂无数据）
 const favoriteCoins = ref([]);
@@ -1086,55 +1009,6 @@ const handleQuickBuy = () => {
     color: #969799;
     text-align: center;
     margin-bottom: 24px;
-  }
-
-  .menu-section {
-    margin-top: 16px;
-
-    .menu-item {
-      border-bottom: 1px solid #ebedf0;
-
-      &:last-child {
-        border-bottom: none;
-      }
-
-      .menu-header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 16px;
-        cursor: pointer;
-        user-select: none;
-
-        .menu-title {
-          font-size: 16px;
-          font-weight: 500;
-          color: #323233;
-        }
-
-        .menu-toggle {
-          font-size: 20px;
-          color: #323233;
-          font-weight: 300;
-          line-height: 1;
-        }
-      }
-
-      .menu-content {
-        padding: 0 16px 16px 16px;
-
-        .menu-sub-item {
-          padding: 12px 0;
-          font-size: 14px;
-          color: #323233;
-          border-bottom: 1px solid #ebedf0;
-
-          &:last-child {
-            border-bottom: none;
-          }
-        }
-      }
-    }
   }
 }
 
