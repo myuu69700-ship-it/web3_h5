7dbc422f5d5e1b758050853ed7c3f7146703b4d5
web-h-002 <web-h-002@gmail.com>
2026-01-10 10:05:31 +0800

    去掉postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,15 @@
 export default {
   plugins: {
-    'postcss-pxtorem': {
-      rootValue: 75, // 设计稿宽度 750px，1rem = 75px
-      propList: ['*'], // 所有属性都转换
-      selectorBlackList: ['.ignore', '.hairlines'], // 忽略的类名
-      replace: true,
-      mediaQuery: false,
-      minPixelValue: 1, // 小于1px的值不转换
-    },
+    // 注释掉 postcss-pxtorem，因为项目主要使用 vw 单位适配
+    // 如果启用会导致 px 和 vw 混用，适配不一致
+    // 'postcss-pxtorem': {
+    //   rootValue: 75, // 设计稿宽度 750px，1rem = 75px
+    //   propList: ['*'], // 所有属性都转换
+    //   selectorBlackList: ['.ignore', '.hairlines'], // 忽略的类名
+    //   replace: true,
+    //   mediaQuery: false,
+    //   minPixelValue: 1, // 小于1px的值不转换
+    // },
   },
 }
 
--- src/main.js
+++ src/main.js
@@ -8,20 +8,21 @@ import App from './App.vue'
 // 全局样式
 import './styles/index.scss'
 
-// 移动端适配：基于 750px 设计稿
-function setRemUnit() {
-  const designWidth = 750 // 设计稿宽度
-  const baseSize = 75 // 基准值：1rem = 75px (750 / 10)
-  const scale = document.documentElement.clientWidth / designWidth
-  document.documentElement.style.fontSize = baseSize * Math.min(scale, 2) + 'px'
-}
+// 移动端适配：项目使用 vw 单位适配，不需要设置 rem
+// 如果使用 rem 适配，请取消下面的注释并启用 postcss-pxtorem
+// function setRemUnit() {
+//   const designWidth = 750 // 设计稿宽度
+//   const baseSize = 75 // 基准值：1rem = 75px (750 / 10)
+//   const scale = document.documentElement.clientWidth / designWidth
+//   document.documentElement.style.fontSize = baseSize * Math.min(scale, 2) + 'px'
+// }
 
 // 初始化
-setRemUnit()
+// setRemUnit()
 // 监听窗口变化
-window.addEventListener('resize', setRemUnit)
+// window.addEventListener('resize', setRemUnit)
 // 监听屏幕旋转
-window.addEventListener('orientationchange', setRemUnit)
+// window.addEventListener('orientationchange', setRemUnit)
 
 const app = createApp(App)
 const pinia = createPinia()
